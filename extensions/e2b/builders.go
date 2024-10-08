bca3ad713d78b843cc03778296433a38c58abccb
conneroisu <conneroisu@outlook.com>
2024-10-07 20:12:08 -0400

    remove formbuilder

--- extensions/e2b/builders.go
+++ extensions/e2b/builders.go
@@ -4,12 +4,8 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"io"
-	"mime/multipart"
 	"net/http"
-	"os"
-	"path"
 )
 
 type (
@@ -56,16 +52,6 @@ func (s *Sandbox) newRequest(
 
 // formBuilder is an interface for building a form.
 type (
-	formBuilder interface {
-		io.Closer
-		CreateFormFile(fieldname string, file *os.File) error
-		CreateFormFileReader(fieldname string, r io.Reader, filename string) error
-		WriteField(fieldname, value string) error
-		FormDataContentType() string
-	}
-	defaultFormBuilder struct {
-		writer *multipart.Writer
-	}
 	requestBuilder interface {
 		Build(
 			ctx context.Context,
@@ -76,60 +62,6 @@ type (
 	}
 	httpRequestBuilder struct{}
 )
-
-// newFormBuilder creates a new DefaultFormBuilder.
-func newFormBuilder(body io.Writer) *defaultFormBuilder {
-	return &defaultFormBuilder{
-		writer: multipart.NewWriter(body),
-	}
-}
-
-func (fb *defaultFormBuilder) CreateFormFile(
-	fieldname string,
-	file *os.File,
-) error {
-	return fb.createFormFile(fieldname, file, file.Name())
-}
-
-func (fb *defaultFormBuilder) CreateFormFileReader(
-	fieldname string,
-	r io.Reader,
-	filename string,
-) error {
-	return fb.createFormFile(fieldname, r, path.Base(filename))
-}
-
-func (fb *defaultFormBuilder) createFormFile(
-	fieldname string,
-	r io.Reader,
-	filename string,
-) error {
-	if filename == "" {
-		return fmt.Errorf("filename cannot be empty")
-	}
-
-	fieldWriter, err := fb.writer.CreateFormFile(fieldname, filename)
-	if err != nil {
-		return err
-	}
-	_, err = io.Copy(fieldWriter, r)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (fb *defaultFormBuilder) WriteField(fieldname, value string) error {
-	return fb.writer.WriteField(fieldname, value)
-}
-
-func (fb *defaultFormBuilder) Close() error {
-	return fb.writer.Close()
-}
-
-func (fb *defaultFormBuilder) FormDataContentType() string {
-	return fb.writer.FormDataContentType()
-}
 
 func newRequestBuilder() *httpRequestBuilder {
 	return &httpRequestBuilder{}
