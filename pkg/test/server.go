09e2228e35629b418ba09ead130358ef7bcbf556
conneroisu <conneroisu@outlook.com>
2024-10-19 18:02:46 -0400

    added toolhouse test server method on the test server

--- pkg/test/server.go
+++ pkg/test/server.go
@@ -5,7 +5,6 @@ import (
 	"log/slog"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"regexp"
 	"strings"
 )
@@ -116,32 +115,38 @@ func (ts *ServerTest) E2bTestServer() *httptest.Server {
 	)
 }
 
-var (
-	defaultLogger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		AddSource: true,
-		Level:     slog.LevelDebug,
-		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
-			if a.Key == "time" {
-				return slog.Attr{}
+// ToolhouseTestServer creates a test server for emulating the toolhouse api.
+func (ts *ServerTest) ToolhouseTestServer() *httptest.Server {
+	return httptest.NewUnstartedServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Printf(
+				"received a %s request at path %q\n",
+				r.Method,
+				r.URL.Path,
+			)
+
+			// check auth
+			if r.Header.Get("Authorization") != "Bearer "+GetTestToken() &&
+				r.Header.Get("api-key") != GetTestToken() {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
 			}
-			if a.Key == "level" {
-				return slog.Attr{}
-			}
-			if a.Key == slog.SourceKey {
-				str := a.Value.String()
-				split := strings.Split(str, "/")
-				if len(split) > 2 {
-					a.Value = slog.StringValue(strings.Join(split[len(split)-2:], "/"))
-					a.Value = slog.StringValue(strings.Replace(a.Value.String(), "}", "", -1))
+
+			// Handle /path/* routes.
+			// Note: the * is converted to a .* in register handler for proper regex handling
+			for route, handler := range ts.handlers {
+				// Adding ^ and $ to make path matching deterministic since go map iteration isn't ordered
+				pattern, _ := regexp.Compile("^" + route + "$")
+				if pattern.MatchString(r.URL.Path) {
+					handler(w, r)
+					return
 				}
-				a.Key = a.Value.String()
-				a.Value = slog.IntValue(0)
 			}
-			if a.Key == "body" {
-				a.Value = slog.StringValue(strings.Replace(a.Value.String(), "/", "", -1))
-				a.Value = slog.StringValue(strings.Replace(a.Value.String(), "\n", "", -1))
-				a.Value = slog.StringValue(strings.Replace(a.Value.String(), "\"", "", -1))
-			}
-			return a
-		}}))
-)
+			http.Error(
+				w,
+				"the resource path doesn't exist",
+				http.StatusNotFound,
+			)
+		}),
+	)
+}
