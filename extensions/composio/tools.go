b7a1e1457c808b0d473f49b802b3994c5a8fcd78
conneroisu <conneroisu@outlook.com>
2024-10-25 11:15:06 -0400

    clean up the error message return for the tooling wrapper

--- extensions/composio/tools.go
+++ extensions/composio/tools.go
@@ -10,44 +10,51 @@ import (
 	"github.com/conneroisu/groq-go/pkg/tools"
 )
 
-// Tool represents a composio tool as returned by the api.
-type Tool struct {
-	Name        string                   `json:"name"`
-	Enum        string                   `json:"enum"`
-	Tags        []string                 `json:"tags"`
-	Logo        string                   `json:"logo"`
-	AppID       string                   `json:"appId"`
-	AppName     string                   `json:"appName"`
-	DisplayName string                   `json:"displayName"`
-	Description string                   `json:"description"`
-	Parameters  tools.FunctionParameters `json:"parameters"`
-	Response    struct {
-		Properties struct {
-			Data struct {
-				Title string `json:"title"`
-				Type  string `json:"type"`
-			} `json:"data"`
-			Successful struct {
-				Description string `json:"description"`
-				Title       string `json:"title"`
-				Type        string `json:"type"`
-			} `json:"successful"`
-			Error struct {
-				AnyOf []struct {
-					Type string `json:"type"`
-				} `json:"anyOf"`
-				Default     any    `json:"default"`
-				Description string `json:"description"`
-				Title       string `json:"title"`
-			} `json:"error"`
-		} `json:"properties"`
-		Required []string `json:"required"`
-		Title    string   `json:"title"`
-		Type     string   `json:"type"`
-	} `json:"response"`
-	Deprecated   bool   `json:"deprecated"`
-	DisplayName0 string `json:"display_name"`
-}
+type (
+	// Tooler is an interface for retreiving composio tools.
+	Tooler interface {
+		GetTools(ctx context.Context, opts ...ToolsOption) (
+			[]tools.Tool, error)
+	}
+	// Tool represents a composio tool as returned by the api.
+	Tool struct {
+		Name        string                   `json:"name"`
+		Enum        string                   `json:"enum"`
+		Tags        []string                 `json:"tags"`
+		Logo        string                   `json:"logo"`
+		AppID       string                   `json:"appId"`
+		AppName     string                   `json:"appName"`
+		DisplayName string                   `json:"displayName"`
+		Description string                   `json:"description"`
+		Parameters  tools.FunctionParameters `json:"parameters"`
+		Response    struct {
+			Properties struct {
+				Data struct {
+					Title string `json:"title"`
+					Type  string `json:"type"`
+				} `json:"data"`
+				Successful struct {
+					Description string `json:"description"`
+					Title       string `json:"title"`
+					Type        string `json:"type"`
+				} `json:"successful"`
+				Error struct {
+					AnyOf []struct {
+						Type string `json:"type"`
+					} `json:"anyOf"`
+					Default     any    `json:"default"`
+					Description string `json:"description"`
+					Title       string `json:"title"`
+				} `json:"error"`
+			} `json:"properties"`
+			Required []string `json:"required"`
+			Title    string   `json:"title"`
+			Type     string   `json:"type"`
+		} `json:"response"`
+		Deprecated   bool   `json:"deprecated"`
+		DisplayName0 string `json:"display_name"`
+	}
+)
 
 // GetTools returns the tools for the composio client.
 func (c *Composio) GetTools(
