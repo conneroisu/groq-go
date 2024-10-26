be39dbe2453ed2c04cb02d866c0ad3d7726b0d58
conneroisu <conneroisu@outlook.com>
2024-10-26 10:33:02 -0400

    added local test to execute_test.go

--- extensions/composio/execute_test.go
+++ extensions/composio/execute_test.go
@@ -2,15 +2,84 @@ package composio
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
 	"os"
 	"testing"
 
 	"github.com/conneroisu/groq-go"
 	"github.com/conneroisu/groq-go/pkg/test"
+	"github.com/conneroisu/groq-go/pkg/tools"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRun(t *testing.T) {
+	a := assert.New(t)
+	ctx := context.Background()
+	ts := test.NewTestServer()
+	ts.RegisterHandler("/v1/connectedAccounts", func(w http.ResponseWriter, _ *http.Request) {
+		var items struct {
+			Items []ConnectedAccount `json:"items"`
+		}
+		items.Items = append(items.Items, ConnectedAccount{
+			IntegrationID:         "INTEGRATION_ID",
+			IsDisabled:            false,
+			ID:                    "ID",
+			MemberID:              "MEMBER_ID",
+			ClientUniqueUserID:    "CLIENT_UNIQUE_USER_ID",
+			Status:                "STATUS",
+			AppUniqueID:           "APP_UNIQUE_ID",
+			AppName:               "APP_NAME",
+			IntegrationIsDisabled: false,
+			InvocationCount:       "INVOCATION_COUNT",
+		})
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		jsonBytes, err := json.Marshal(items)
+		a.NoError(err)
+		_, err = w.Write(jsonBytes)
+		a.NoError(err)
+	})
+	ts.RegisterHandler("/v2/actions/TOOL/execute", func(w http.ResponseWriter, _ *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(`response1`))
+		a.NoError(err)
+	})
+	testS := ts.ComposioTestServer()
+	testS.Start()
+	client, err := NewComposer(
+		test.GetTestToken(),
+		WithLogger(test.DefaultLogger),
+		WithBaseURL(testS.URL),
+	)
+	a.NoError(err)
+	resp, err := client.Run(ctx, groq.ChatCompletionResponse{
+		Choices: []groq.ChatCompletionChoice{
+			{
+				Message: groq.ChatCompletionMessage{
+					Role:    groq.ChatMessageRoleUser,
+					Content: "Hello!",
+					ToolCalls: []tools.ToolCall{
+						{
+							Function: tools.FunctionCall{
+								Name: "TOOL",
+								Arguments: `{ 
+									"foo": "bar",
+								}`,
+							},
+						},
+					},
+				},
+				FinishReason: groq.FinishReasonFunctionCall,
+			},
+		},
+	})
+	a.NoError(err)
+	assert.Equal(t, "response1", resp[0].Content)
+}
+
+func TestUnitRun(t *testing.T) {
 	if !test.IsUnitTest() {
 		t.Skip()
 	}
