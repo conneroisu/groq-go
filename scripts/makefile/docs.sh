a785119440ce12d9a6344f9b28050e2b04c3203d
connerohnesorge <conneroisu@outlook.com>
2024-12-16 10:14:31 -0600

    remove taskfile and makefile scripts in favor of devenv

--- scripts/makefile/docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Name: makefile.docs.sh
-# Url: 
-# 
-# Description: A script to generate the go docs for the project.
-# 
-# Usage: make docs
-
-gomarkdoc -o README.md -e .
--- scripts/taskfile/docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Name: makefile/docs.sh
-# Description: A script to generate the go docs for the project.
-# 
-# Usage: make docs
-
-gum spin --spinner dot --title "Generating Docs" --show-output -- \
-    gomarkdoc -o README.md -e .
