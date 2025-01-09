2b54d99cbdfb1e22990c709c2a956922834c3be6
connerohnesorge <conneroisu@outlook.com>
2025-01-09 13:35:55 -0600

    added gomarkdoc to devenv and added a README.md to the tools package that is generated from the gomarkdoc comments in doc.go

--- devenv.nix
+++ devenv.nix
@@ -38,6 +38,7 @@ in {
     gomodifytags
     gotests
     gotools
+    gomarkdoc
   ];
 
   scripts = {
--- pkg/tools/doc.go
+++ pkg/tools/doc.go
@@ -1,2 +1,4 @@
 // Package tools contains the interfaces for groq-go tooling usable by llms.
 package tools
+
+//go:generate gomarkdoc -o README.md -e .
