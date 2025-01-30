bb83a20746c7846fc4f8238ef8f11897bfc3e9af
connerohnesorge <conneroisu@outlook.com>
2025-01-30 12:42:22 -0600

    upgrade dev env to nix flakes from devenv

---
+++ flake.nix
@@ -0,0 +1,88 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    devenv.url = "github:cachix/devenv";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs @ {
+    flake-parts,
+    devenv-root,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = ["x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devenv.shells.default = {
+          devenv.root = let
+            devenvRootFileContent = builtins.readFile devenv-root.outPath;
+          in
+            pkgs.lib.mkIf (devenvRootFileContent != "") devenvRootFileContent;
+
+          name = "groq-go";
+
+          imports = [
+          ];
+
+          languages.go = {
+            enable = true;
+            package = pkgs.go;
+          };
+
+          packages = with pkgs; [
+            # Go Tools
+            air
+            templ
+            podman
+            esbuild
+            buf
+            pprof
+            golangci-lint
+            golangci-lint-langserver
+            revive
+            gomarkdoc
+            gotests
+            gotools
+            reftools
+          ];
+
+          enterShell = ''
+            export REPO_ROOT=$(git rev-parse --show-toplevel)
+          '';
+
+          scripts = {
+            generate.exec = ''
+              go generate -v ./...
+            '';
+          };
+        };
+      };
+      flake = {
+      };
+    };
+}
